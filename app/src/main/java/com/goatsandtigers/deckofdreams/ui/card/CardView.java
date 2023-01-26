54c2b3c8ece348be4b01c9b5df210915b18f987a
dan <danielnewman106@hotmail.com>
2023-01-26 21:31:27 +0000

    Card cost displayed in top-left-hand corner of CardView.

--- app/src/main/java/com/goatsandtigers/deckofdreams/ui/card/CardView.java
+++ app/src/main/java/com/goatsandtigers/deckofdreams/ui/card/CardView.java
@@ -1,9 +1,11 @@
 package com.goatsandtigers.deckofdreams.ui.card;
 
 import android.content.Context;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.goatsandtigers.deckofdreams.R;
 import com.goatsandtigers.deckofdreams.cards.Card;
 
 public class CardView extends LinearLayout {
@@ -15,9 +17,11 @@ public class CardView extends LinearLayout {
     public CardView(Context context, Card card) {
         super(context);
         this.card = card;
+        setOrientation(VERTICAL);
         setLayoutParams(new LinearLayout.LayoutParams(WIDTH, 200));
         setPadding(8, 8, 8, 8);
         setBackgroundColor(card.getColor());
+        displayCost();
         addView(buildNameTextView());
     }
 
@@ -25,6 +29,22 @@ public class CardView extends LinearLayout {
         TextView tv = new TextView(getContext());
         tv.setText(card.getName());
         return tv;
+    }
+
+    private void displayCost() {
+        if (card.getCost() > 0) {
+            LinearLayout costLayout = new LinearLayout(getContext());
+            ImageView costImageView = new ImageView(getContext());
+            costImageView.setImageResource(R.drawable.cost);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(50, 50);
+            costImageView.setLayoutParams(params);
+            costLayout.addView(costImageView);
+            TextView tv = new TextView(getContext());
+            String costString = String.valueOf(card.getCost());
+            tv.setText(costString);
+            costLayout.addView(tv);
+            addView(costLayout);
+        }
     }
 
     public Card getCard() {
